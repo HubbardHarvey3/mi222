9a41da0876814fec38f4aec3fc27c99969d06273
HubbardHarvey3 <hubbard.harvey@gmail.com>
2018-07-26 21:40:27 -0400

    add scroll button on screen with styling

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -1,5 +1,10 @@
-
-    // Animated Jumbotron
+//  JS for scrolling button
+let scroll = document.getElementById("scrollTop");
+scroll.addEventListener('click', function(){
+  scrollTo({top: 0,
+    behavior: "smooth"})
+});
+// Animated Jumbotron
 
 let i = 0;
 let txt = "The Hernandez Family, Missionaries to Guatemala."
@@ -10,8 +15,7 @@ function animate () {
         document.getElementById("animateHeader").innerHTML += txt.charAt(i);
         i++;
         setTimeout(animate, speed);
-        
+
     }
 };
 animate();
-
