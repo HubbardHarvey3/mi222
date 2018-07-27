8012ed836134b61f361471da5291ab97e971c071
HubbardHarvey3 <hubbard.harvey@gmail.com>
2018-07-26 20:07:22 -0400

    adding to repo from local

--- app.js
+++ app.js
@@ -5,25 +5,25 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(req, res) {
-   res.render('../public/views/index.ejs') 
+   res.render('../public/views/index.ejs')
 });
 
 app.get('/statment', function(req, res) {
-   res.render('../public/views/SoF.ejs') 
+   res.render('../public/views/SoF.ejs')
 });
 
 app.get('/about', function(req, res) {
-   res.render('../public/views/about.ejs') 
+   res.render('../public/views/about.ejs')
 });
 
 app.get('/contact', function(req, res) {
-   res.render('../public/views/contact.ejs') 
+   res.render('../public/views/contact.ejs')
 });
 
 app.get('/blog', function(req, res) {
-   res.render('../public/views/blog.ejs') 
+   res.render('../public/views/blog.ejs')
 });
 
-app.listen(process.env.PORT, process.env.IP, function(){
+app.listen(3000, function(){
    console.log("Blog server is spinning up");
 });
