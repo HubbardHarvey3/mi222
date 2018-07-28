c8dbeb5eff16aaaa0831cde3e57b49126e991037
HubbardHarvey3 <hubbard.harvey@gmail.com>
2018-07-28 00:05:34 -0400

    add js to fade top button on scroll

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -1,9 +1,26 @@
 //  JS for scrolling button
 let scroll = document.getElementById("scrollTop");
+let scrollTopContainer = document.getElementById("scrollTopContainer");
 scroll.addEventListener('click', function(){
   scrollTo({top: 0,
     behavior: "smooth"})
 });
+
+// JS for detecting scrollY position and adding opacity to Top Button
+
+ window.addEventListener('scroll', function(){
+   let topbutton = window.scrollY
+   if (topbutton > 0) {
+     scrollTopContainer.classList.add("show");
+     scrollTopContainer.classList.remove("hidden");
+   } else {
+     scrollTopContainer.classList.remove("show")
+     scrollTopContainer.classList.add("hidden")
+   }
+ })
+
+
+
 // Animated Jumbotron
 
 let i = 0;
