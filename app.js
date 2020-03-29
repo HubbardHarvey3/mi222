d87143d84f565dbfdc038b5fa21b4a71f9c83961
HubbardHarvey3 <hubbard.harvey@gmail.com>
2020-03-29 13:34:20 -0400

    Update to KJV and add radio page

--- app.js
+++ app.js
@@ -20,52 +20,56 @@ let mailer = require('nodemailer').createTransport({
   port: 465,
   secure: true,
   auth: {
-    user:process.env.USER,
+    user: process.env.USER,
     pass: process.env.PASSWORD
   }
 })
 
-app.get('/contact', function(req,res){
+app.get('/contact', function (req, res) {
   res.render('../public/views/contact.ejs', {
-    data : {},
-    errors : {}
+    data: {},
+    errors: {}
   })
 
 })
 
-app.post ('/contact', function(req,res){
-    req.body.message = req.sanitize(req.body.message)
-    req.body.sender = req.sanitize(req.body.sender)
-    mailer.sendMail({
-      from:process.env.USER,
-      to:process.env.USER,
-      message:req.body.message,
-      subject:req.body.sender,
-      html:req.body.message,
-    }), function(err, info){
-      if (err) return res.status(500).send(err);
-      res.json({success: true});
-    }
-    res.render('../public/views/success.ejs')
+app.post('/contact', function (req, res) {
+  req.body.message = req.sanitize(req.body.message)
+  req.body.sender = req.sanitize(req.body.sender)
+  mailer.sendMail({
+    from: process.env.USER,
+    to: process.env.USER,
+    message: req.body.message,
+    subject: req.body.sender,
+    html: req.body.message,
+  }), function (err, info) {
+    if (err) return res.status(500).send(err);
+    res.json({ success: true });
+  }
+  res.render('../public/views/success.ejs')
 });
 
 // ROUTES
-app.get('/', function(req, res) {
-   res.render('../public/views/index.ejs')
+app.get('/', function (req, res) {
+  res.render('../public/views/index.ejs')
 });
 
-app.get('/statement', function(req, res) {
-   res.render('../public/views/SoF.ejs')
+app.get('/statement', function (req, res) {
+  res.render('../public/views/SoF.ejs')
 });
 
-app.get('/about', function(req, res) {
-   res.render('../public/views/about.ejs')
+app.get('/about', function (req, res) {
+  res.render('../public/views/about.ejs')
 });
 
-app.get('/blog', function(req, res) {
-   res.render('../public/views/blog.ejs')
+app.get('/blog', function (req, res) {
+  res.render('../public/views/blog.ejs')
 });
 
-app.listen(port, function(){
-   console.log("Blog server is spinning up");
+app.get('/radio', function (req, res) {
+  res.render('../public/views/radio.ejs')
+});
+
+app.listen(port, function () {
+  console.log("Blog server is spinning up on " + port);
 });
